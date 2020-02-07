72e27cd46ce6f24d938826b611625a45f3f1de6b
MIkhail-Klashkin <52420553+MIkhail-Klashkin@users.noreply.github.com>
2020-02-07 13:21:19 +0400

    Ёбаный рот
    этого гита

---
+++ TrainsAppl/DAL/TrainDBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TrainsApp.Models;
+using TrainsApp.Models.DB;
+
+namespace TrainsApp.DAL
+{
+    public class TrainDBContext:DbContext
+    {
+        static TrainDBContext()
+        {
+            Database.SetInitializer(new TrainDBInitializer());
+        }
+
+        public TrainDBContext() : base("TrainDBContext")
+        {
+
+        }
+
+        public DbSet<UserDB> Users { get; set; }
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<Timetable> Timetables { get; set; }
+        public DbSet<Train> Trains { get; set; }
+
+        //   public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ TrainsAppl/Interfaces/ILoginView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainsAppl.Interfaces
+{
+    interface ILoginView
+    {
+        string Username { get; }
+        string Password { get; }
+        event Action Login;      
+        void ShowError(string errorMessage);
+    }
+}
